824fd7d389c9fc6029cd7ca45d8a0797af7b9332
Nick Quaranto <nick@quaran.to>
2011-03-06 23:11:35 -0500

    Fix gears and add more pictures

--- javascripts/gear.js
+++ javascripts/gear.js
@@ -11,13 +11,13 @@ $(window).load(function() {
     gear.animate({rotation: deg * 1000}, 20000 * 1000);
   }
 
-  var gear1 = drawGear(10, 10, 250, 0.1);
-  var gear2 = drawGear(228, 200, 75, 0.2);
-  var gear3 = drawGear(210, -450, 700, 0.25);
+  var gear1 = drawGear(10, 10, 250, 0.1);     // medium gear
+  var gear2 = drawGear(228, 200, 75, 0.2);    // tiny gear
+  var gear3 = drawGear(210, -450, 700, 0.25); // big gear
 
   if(!$.browser.msie) {
-    rotateGear(gear1, 360);
-    rotateGear(gear2, -360);
+    rotateGear(gear1, -360);
+    rotateGear(gear2, 360);
     rotateGear(gear3, 360);
   }
 
